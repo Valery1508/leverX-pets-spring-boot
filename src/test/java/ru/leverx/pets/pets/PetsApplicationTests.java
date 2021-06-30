b7c14c6382e80310b7d5f0519170a2aed97aea4f
Valery1508 <lera.zubritskaya@gmail.com>
2021-06-30 16:03:53 +0300

    Added json logs on cf and fixed bugs

--- src/test/java/ru/leverx/pets/pets/PetsApplicationTests.java
+++ src/test/java/ru/leverx/pets/pets/PetsApplicationTests.java
@@ -2,8 +2,10 @@ package ru.leverx.pets.pets;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(value = "dev")
 class PetsApplicationTests {
 
     @Test
